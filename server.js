32cd0eac71169e1d1dbdc6656fe984c7b84f6137
Scott Hagen <shagen@keste.com>
2018-08-07 10:29:04 -0500

    trying some more server js stuff

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/Angular', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
 });
 
+var databaseurl = 'postgres://nswclifegblitx:597cfe4bfb7502885f6404f82189812c41b268fda0bb9ed5c6ac266db2b11b91@ec2-54-243-54-6.compute-1.amazonaws.com:5432/d5ng6knqu0upae'
+
+console.log('the database url is' + process.env.DATABASE_URL);
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
