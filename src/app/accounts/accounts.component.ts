51c1be02e79590d690c4e61db874e7dbb7a2c8cf
Scott Hagen <shagen@keste.com>
2018-08-08 11:14:57 -0500

    added accounts respone shown to UI

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountsResp, AccountsService } from './accounts.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
@@ -9,16 +12,25 @@ import { AccountsResp, AccountsService } from './accounts.service';
 export class AccountsComponent implements OnInit {
 
     accountsResp : AccountsResp;
+    data : Observable<any>;
+    baseUrl : String;
+  constructor(private accountsService: AccountsService, private http: HttpClient) { }
 
-  constructor(private accountsService: AccountsService) { }
-
+    
   ngOnInit() {
+        var getUrl = window.location;
+        this.baseUrl = getUrl .protocol + "//" + getUrl.host;
+        console.log(this.baseUrl);
   }
 
     getAccounts(){
         //this.accountsService.getAccounts()
         //    .subscribe( accountsResp => this.accountsResp = accountsResp);
             
+    }
+
+    getData(){
+        this.data = this.http.get(this.baseUrl + '/accounts');
     }
 
 }
