c4c55adc2bf61c08121f2054ebabc264173d6284
Scott Hagen <shagen@keste.com>
2018-08-08 15:33:17 -0500

    added ability to edit fields in the UI

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountsResp, AccountsService } from './accounts.service';
-import { Observable } from 'rxjs';
+import { Observable ,Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -10,21 +10,31 @@ import { HttpClient } from '@angular/common/http';
   providers: [AccountsService],
   styleUrls: ['./accounts.component.css']
 })
+
+
 export class AccountsComponent implements OnInit {
 
+    testVar : String;
     accountsResp : AccountsResp;
-    data : Observable<any>;
+    dataFromSF : Observable<any>
+    data : String;
     baseUrl : String;
+    editMode : boolean;
+
   constructor(private accountsService: AccountsService, private http: HttpClient) { }
 
-    
+  
   ngOnInit() {
+        this.testVar = "ABCDEFG";
+        this.editMode = false;
         var getUrl = window.location;
         this.baseUrl = getUrl .protocol + "//" + getUrl.host;
         console.log(this.baseUrl);
         this.getData();
   }
-
+    toggleEditMode(){
+        this.editMode = !this.editMode;
+    }
     getAccounts(){
         //this.accountsService.getAccounts()
         //    .subscribe( accountsResp => this.accountsResp = accountsResp);
@@ -32,7 +42,15 @@ export class AccountsComponent implements OnInit {
     }
 
     getData(){
-        this.data = this.http.get(this.baseUrl + '/accounts');
+        this.dataFromSF = this.http.get(this.baseUrl + '/accounts');
+        this.dataFromSF.subscribe(val => this.data = val);
     }
 
+    trackByIndex(index: number, obj: any): any {
+      return index;
+    }
+
+    loadValueChange(event,index,field){
+        console.log("Event is " + event + " " + index + " " + field);
+    }
 }
