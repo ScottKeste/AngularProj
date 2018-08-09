95036f65f960be96f3f8e76eb62dcbd88694424d
Scott Hagen <shagen@keste.com>
2018-08-09 11:38:39 -0500

    added functionality to allow saving data back to heroku postgres

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
 
+app.use(bodyParser.json());
+
 app.get('/Angular', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
@@ -24,7 +26,7 @@ app.get('/accounts', function(req,res) {
             console.log(err);
             }else{	
                 conn.query(
-                    'select * from salesforce.account;',
+                    'select * from salesforce.account order by name asc;',
                     function(err, result) {
                         if (err != null || result.rowCount == 0) {
                             console.log('was an error');
@@ -38,6 +40,22 @@ app.get('/accounts', function(req,res) {
                 );
             }
     });
+});
+
+app.post('/update', function(req,res){
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        var arrayLength = req.body.length;
+        for(var i = 0; i < arrayLength; i++){
+            var account = req.body[i];
+            conn.query(
+                    'UPDATE Salesforce.Account SET billingcountry = $2, billingstate = $3, billingstreet = $4, phone = $5 WHERE sfid = $1;',
+                    [account.sfid, account.billingcountry, account.billingstate, account.billingstreet, account.phone]
+                    );
+        }
+        done();
+    });
+    
+    res.send('{"val" : "bbbb"}');
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AccountsResp, AccountsService } from './accounts.service';
 import { Observable ,Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 
 
 @Component({
@@ -17,7 +25,8 @@ export class AccountsComponent implements OnInit {
     testVar : String;
     accountsResp : AccountsResp;
     dataFromSF : Observable<any>
-    data : String;
+    dataPost : Observable<any>
+    data : string;
     baseUrl : String;
     editMode : boolean;
 
@@ -44,6 +53,14 @@ export class AccountsComponent implements OnInit {
     getData(){
         this.dataFromSF = this.http.get(this.baseUrl + '/accounts');
         this.dataFromSF.subscribe(val => this.data = val);
+    }
+
+    updateAccounts(){
+        //console.log(JSON.parse(JSON.stringify(this.data)).rows);
+        //console.log(JSON.stringify(this.data));
+        //console.log(this.baseUrl + '/update');
+        this.dataPost = this.http.post(this.baseUrl + '/update', JSON.parse(JSON.stringify(this.data)).rows, httpOptions);
+        this.dataPost.subscribe(val => console.log('got post response ');
     }
 
     trackByIndex(index: number, obj: any): any {
